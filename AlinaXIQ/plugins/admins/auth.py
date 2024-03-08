9d343aae332f4bcbae7aa2b8120ca27615bb8bda
iox7ssz <159122521+iox7ssz@users.noreply.github.com>
2024-03-08 21:58:59 +0300

    Update auth.py

--- AlinaXIQ/plugins/admins/auth.py
+++ AlinaXIQ/plugins/admins/auth.py
@@ -1,29 +1,18 @@
-# Copyright (C) 2024 by Alexa_Help @ Github, < https://github.com/TheTeamAlexa >
-# Subscribe On YT < Jankari Ki Duniya >. All rights reserved. © Alexa © Yukki.
-
-""""
-TheTeamAlexa is a project of Telegram bots with variety of purposes.
-Copyright (c) 2024 -present Team=Alexa <https://github.com/TheTeamAlexa>
-
-This program is free software: you can redistribute it and can modify
-as you want or you can collabe if you have new ideas.
-"""
-
 
 from pyrogram import filters
 from pyrogram.types import Message
 
 from config import BANNED_USERS, adminlist
 from strings import get_command
-from AlexaMusic import app
-from AlexaMusic.utils.database import (
+from AlinaXIQ import app
+from AlinaXIQ.utils.database import (
     delete_authuser,
     get_authuser,
     get_authuser_names,
     save_authuser,
 )
-from AlexaMusic.utils.decorators import AdminActual, language
-from AlexaMusic.utils.formatters import int_to_alpha
+from AlinaXIQ.utils.decorators import AdminActual, language
+from AlinaXIQ.utils.formatters import int_to_alpha
 
 # Command
 AUTH_COMMAND = get_command("AUTH_COMMAND")
