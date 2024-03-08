7827c81aa1dad2d4db3f420e42ee27e88d63eaa7
iox7ssz <159122521+iox7ssz@users.noreply.github.com>
2024-03-08 23:23:10 +0300

    Update maintenance.py

--- AlinaXIQ/plugins/sudo/maintenance.py
+++ AlinaXIQ/plugins/sudo/maintenance.py
@@ -1,28 +1,16 @@
-# Copyright (C) 2024 by Alexa_Help @ Github, < https://github.com/TheTeamAlexa >
-# Subscribe On YT < Jankari Ki Duniya >. All rights reserved. © Alexa © Yukki.
-
-""""
-TheTeamAlexa is a project of Telegram bots with variety of purposes.
-Copyright (c) 2024 -present Team=Alexa <https://github.com/TheTeamAlexa>
-
-This program is free software: you can redistribute it and can modify
-as you want or you can collabe if you have new ideas.
-"""
-
-
 from pyrogram import filters
 from pyrogram.types import Message
 
 from strings import get_command, get_string
-from AlexaMusic import app
-from AlexaMusic.misc import SUDOERS
-from AlexaMusic.utils.database import (
+from AlinaXIQ import app
+from AlinaXIQ.misc import SUDOERS
+from AlinaXIQ.utils.database import (
     get_lang,
     is_maintenance,
     maintenance_off,
     maintenance_on,
 )
-from AlexaMusic.utils.decorators.language import language
+from AlinaXIQ.utils.decorators.language import language
 
 # Commands
 MAINTENANCE_COMMAND = get_command("MAINTENANCE_COMMAND")
