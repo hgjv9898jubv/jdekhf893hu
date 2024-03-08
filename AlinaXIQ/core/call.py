a9ebec535a28d64525d39f1be87c567c026d5b89
iox7ssz <159122521+iox7ssz@users.noreply.github.com>
2024-03-08 21:35:43 +0300

    Update call.py

--- AlinaXIQ/core/call.py
+++ AlinaXIQ/core/call.py
@@ -1,25 +1,3 @@
-# Copyright (C) 2024 by Alexa_Help @ Github, < https://github.com/TheTeamAlexa >
-# Subscribe On YT < Jankari Ki Duniya >. All rights reserved. © Alexa © Yukki.
-
-""""
-TheTeamAlexa is a project of Telegram bots with variety of purposes.
-Copyright (c) 2024 -present Team=Alexa <https://github.com/TheTeamAlexa>
-
-This program is free software: you can redistribute it and can modify
-as you want or you can collabe if you have new ideas.
-"""
-
-
-# Copyright (C) 2024 by Alexa_Help @ Github, < https://github.com/TheTeamAlexa >
-# Subscribe On YT < Jankari Ki Duniya >. All rights reserved. © Alexa © Yukki.
-
-""""
-TheTeamAlexa is a project of Telegram bots with variety of purposes.
-Copyright (c) 2024 -present Team=Alexa <https://github.com/TheTeamAlexa>
-
-This program is free software: you can redistribute it and can modify
-as you want or you can collabe if you have new ideas.
-"""
 
 
 import asyncio
@@ -48,9 +26,9 @@ from pytgcalls.types import (
 from pytgcalls.types.stream import StreamAudioEnded
 
 import config
-from AlexaMusic import LOGGER, YouTube, app
-from AlexaMusic.misc import db
-from AlexaMusic.utils.database import (
+from AlinaXIQ import LOGGER, YouTube, app
+from AlinaXIQ.misc import db
+from AlinaXIQ.utils.database import (
     add_active_chat,
     add_active_video_chat,
     get_assistant,
@@ -65,11 +43,11 @@ from AlexaMusic.utils.database import (
     remove_active_video_chat,
     set_loop,
 )
-from AlexaMusic.utils.exceptions import AssistantErr
-from AlexaMusic.utils.inline.play import stream_markup, telegram_markup
-from AlexaMusic.utils.stream.autoclear import auto_clean
-from AlexaMusic.utils.thumbnails import gen_thumb
-from AlexaMusic.utils.theme import check_theme
+from AlinaXIQ.utils.exceptions import AssistantErr
+from AlinaXIQ.utils.inline.play import stream_markup, telegram_markup
+from AlinaXIQ.utils.stream.autoclear import auto_clean
+from AlinaXIQ.utils.thumbnails import gen_thumb
+from AlinaXIQ.utils.theme import check_theme
 from strings import get_string
 
 autoend = {}
@@ -86,7 +64,7 @@ async def _clear_(chat_id):
 class Call(PyTgCalls):
     def __init__(self):
         self.userbot1 = Client(
-            name="Alexa1",
+            name="Alina1",
             api_id=config.API_ID,
             api_hash=config.API_HASH,
             session_string=str(config.STRING1),
@@ -96,7 +74,7 @@ class Call(PyTgCalls):
             cache_duration=100,
         )
         self.userbot2 = Client(
-            name="Alexa2",
+            name="Alina2",
             api_id=config.API_ID,
             api_hash=config.API_HASH,
             session_string=str(config.STRING2),
@@ -106,7 +84,7 @@ class Call(PyTgCalls):
             cache_duration=100,
         )
         self.userbot3 = Client(
-            name="Alexa3",
+            name="Alina3",
             api_id=config.API_ID,
             api_hash=config.API_HASH,
             session_string=str(config.STRING3),
@@ -116,7 +94,7 @@ class Call(PyTgCalls):
             cache_duration=100,
         )
         self.userbot4 = Client(
-            name="Alexa4",
+            name="Alina4",
             api_id=config.API_ID,
             api_hash=config.API_HASH,
             session_string=str(config.STRING4),
@@ -126,7 +104,7 @@ class Call(PyTgCalls):
             cache_duration=100,
         )
         self.userbot5 = Client(
-            name="Alexa5",
+            name="Alina5",
             api_id=config.API_ID,
             api_hash=config.API_HASH,
             session_string=str(config.STRING5),
@@ -691,4 +669,4 @@ class Call(PyTgCalls):
                 autoend[chat_id] = {}
 
 
-Alexa = Call()
+Alina = Call()
