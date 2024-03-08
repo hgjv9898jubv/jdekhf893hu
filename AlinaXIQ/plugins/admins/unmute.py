2b239a09ab9ff48b38da90964a1e391f60e43598
iox7ssz <159122521+iox7ssz@users.noreply.github.com>
2024-03-08 22:01:07 +0300

    Update unmute.py

--- AlinaXIQ/plugins/admins/unmute.py
+++ AlinaXIQ/plugins/admins/unmute.py
@@ -15,10 +15,10 @@ from pyrogram.types import Message
 
 from config import BANNED_USERS
 from strings import get_command
-from AlexaMusic import app
-from AlexaMusic.core.call import Alexa
-from AlexaMusic.utils.database import is_muted, mute_off
-from AlexaMusic.utils.decorators import AdminRightsCheck
+from AlinaXIQ import app
+from AlinaXIQ.core.call import Alina
+from AlinaXIQ.utils.database import is_muted, mute_off
+from AlinaXIQ.utils.decorators import AdminRightsCheck
 
 # Commands
 UNMUTE_COMMAND = get_command("UNMUTE_COMMAND")
@@ -32,7 +32,7 @@ async def unmute_admin(Client, message: Message, _, chat_id):
     if not await is_muted(chat_id):
         return await message.reply_text(_["admin_7"], disable_web_page_preview=True)
     await mute_off(chat_id)
-    await Alexa.unmute_stream(chat_id)
+    await Alina.unmute_stream(chat_id)
     await message.reply_text(
         _["admin_8"].format(message.from_user.mention), disable_web_page_preview=True
     )
