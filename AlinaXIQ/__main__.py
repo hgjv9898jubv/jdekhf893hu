53e7f5d99b231d7c14f3ed7848f7b1c59c6239e1
iox7ssz <159122521+iox7ssz@users.noreply.github.com>
2024-03-09 00:33:36 +0300

    Update __main__.py

--- AlinaXIQ/__main__.py
+++ AlinaXIQ/__main__.py
@@ -36,7 +36,7 @@ async def init():
         pass
     await app.start()
     for all_module in ALL_MODULES:
-        importlib.import_module("AlexaMusic.plugins" + all_module)
+        importlib.import_module("AlinaXIQ.plugins" + all_module)
     LOGGER("AlinaXIQ.plugins").info("Necessary Modules Imported Successfully.")
     await userbot.start()
     await Alina.start()
