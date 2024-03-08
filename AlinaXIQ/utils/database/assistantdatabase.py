ac5d0c0d1fa6b0349dbb4a500e05653f4c5569f4
iox7ssz <159122521+iox7ssz@users.noreply.github.com>
2024-03-09 01:04:10 +0300

    Update assistantdatabase.py

--- AlinaXIQ/utils/database/assistantdatabase.py
+++ AlinaXIQ/utils/database/assistantdatabase.py
@@ -33,7 +33,7 @@ async def get_client(assistant: int):
 
 
 async def set_assistant(chat_id):
-    from AlexaMusic.core.userbot import assistants
+    from AlinaXIQ.core.userbot import assistants
 
     ran_assistant = random.choice(assistants)
     assistantdict[chat_id] = ran_assistant
@@ -47,7 +47,7 @@ async def set_assistant(chat_id):
 
 
 async def get_assistant(chat_id: int) -> str:
-    from AlexaMusic.core.userbot import assistants
+    from AlinaXIQ.core.userbot import assistants
 
     assistant = assistantdict.get(chat_id)
     if not assistant:
@@ -74,7 +74,7 @@ async def get_assistant(chat_id: int) -> str:
 
 
 async def set_calls_assistant(chat_id):
-    from AlexaMusic.core.userbot import assistants
+    from AlinaXIQ.core.userbot import assistants
 
     ran_assistant = random.choice(assistants)
     assistantdict[chat_id] = ran_assistant
@@ -87,7 +87,7 @@ async def set_calls_assistant(chat_id):
 
 
 async def group_assistant(self, chat_id: int) -> int:
-    from AlexaMusic.core.userbot import assistants
+    from AlinaXIQ.core.userbot import assistants
 
     assistant = assistantdict.get(chat_id)
     if not assistant:
