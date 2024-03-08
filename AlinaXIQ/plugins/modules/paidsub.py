983f2941768f2f100db3dd6e8916dd0970972fc5
iox7ssz <159122521+iox7ssz@users.noreply.github.com>
2024-03-08 23:13:46 +0300

    Update paidsub.py

--- AlinaXIQ/plugins/modules/paidsub.py
+++ AlinaXIQ/plugins/modules/paidsub.py
@@ -11,13 +11,13 @@ as you want or you can collabe if you have new ideas.
 
 
 import asyncio
-from AlexaMusic import app
+from AlinaXIQ import app
 from pyrogram import Client, filters
 from datetime import datetime, timedelta
 from pyrogram.errors import FloodWait
-from AlexaMusic.core.mongo import db as alexa
+from AlinaXIQ.core.mongo import db as Alina
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
-from AlexaMusic.utils.database import get_served_users, get_served_chats
+from AlinaXIQ.utils.database import get_served_users, get_served_chats
 
 
 OWNER_ID = 6174058850
