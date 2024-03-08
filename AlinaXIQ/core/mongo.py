912a209b30db45b3ad37a0523fc518de3a9b9257
iox7ssz <159122521+iox7ssz@users.noreply.github.com>
2024-03-08 21:37:29 +0300

    Update mongo.py

--- AlinaXIQ/core/mongo.py
+++ AlinaXIQ/core/mongo.py
@@ -1,13 +1,3 @@
-# Copyright (C) 2024 by Alexa_Help @ Github, < https://github.com/TheTeamAlexa >
-# Subscribe On YT < Jankari Ki Duniya >. All rights reserved. © Alexa © Yukki.
-
-""""
-TheTeamAlexa is a project of Telegram bots with variety of purposes.
-Copyright (c) 2024 -present Team=Alexa <https://github.com/TheTeamAlexa>
-
-This program is free software: you can redistribute it and can modify
-as you want or you can collabe if you have new ideas.
-"""
 
 from motor.motor_asyncio import AsyncIOMotorClient as _mongo_client_
 from pymongo import MongoClient
@@ -23,7 +13,7 @@ TEMP_MONGODB = ""
 if config.MONGO_DB_URI is None:
     LOGGER(__name__).warning("No MONGO DB URL found.")
     temp_client = Client(
-        "Alexa",
+        "Alina",
         bot_token=config.BOT_TOKEN,
         api_id=config.API_ID,
         api_hash=config.API_HASH,
@@ -39,8 +29,8 @@ if config.MONGO_DB_URI is None:
 else:
     _mongo_async_ = _mongo_client_(config.MONGO_DB_URI)
     _mongo_sync_ = MongoClient(config.MONGO_DB_URI)
-    mongodb = _mongo_async_.Alexa
-    pymongodb = _mongo_sync_.Alexa
+    mongodb = _mongo_async_.Alina
+    pymongodb = _mongo_sync_.Alina
 
 ## Database For Broadcast Subscription By Team Alexa
 
