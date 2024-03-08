de132ffa8b66c4bed7ef128dc77a6ef30cb6e1d5
iox7ssz <159122521+iox7ssz@users.noreply.github.com>
2024-03-08 21:34:08 +0300

    Update bot.py

--- AlinaXIQ/core/bot.py
+++ AlinaXIQ/core/bot.py
@@ -1,13 +1,3 @@
-# Copyright (C) 2024 by Alexa_Help @ Github, < https://github.com/TheTeamAlexa >
-# Subscribe On YT < Jankari Ki Duniya >. All rights reserved. © Alexa © Yukki.
-
-""""
-TheTeamAlexa is a project of Telegram bots with variety of purposes.
-Copyright (c) 2024 -present Team=Alexa <https://github.com/TheTeamAlexa>
-
-This program is free software: you can redistribute it and can modify
-as you want or you can collabe if you have new ideas.
-"""
 
 
 import sys
@@ -18,7 +8,7 @@ from ..logging import LOGGER
 from pyrogram.enums import ChatMemberStatus
 
 
-class AlexaBot(Client):
+class AlinaBot(Client):
     def __init__(self):
         super().__init__(
             "MusicBot",
