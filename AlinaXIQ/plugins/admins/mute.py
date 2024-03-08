dcb26283dca0eb4ec2ca8de2c6211d8debae1fbb
iox7ssz <159122521+iox7ssz@users.noreply.github.com>
2024-03-08 22:01:38 +0300

    Update mute.py

--- AlinaXIQ/plugins/admins/mute.py
+++ AlinaXIQ/plugins/admins/mute.py
@@ -15,10 +15,10 @@ from pyrogram.types import Message
 
 from config import BANNED_USERS
 from strings import get_command
-from AlexaMusic import app
-from AlexaMusic.core.call import Alexa
-from AlexaMusic.utils.database import is_muted, mute_on
-from AlexaMusic.utils.decorators import AdminRightsCheck
+from AlinaXIQ import app
+from AlinaXIQ.core.call import Alina
+from AlinaXIQ.utils.database import is_muted, mute_on
+from AlinaXIQ.utils.decorators import AdminRightsCheck
 
 # Commands
 MUTE_COMMAND = get_command("MUTE_COMMAND")
@@ -32,7 +32,7 @@ async def mute_admin(cli, message: Message, _, chat_id):
     if await is_muted(chat_id):
         return await message.reply_text(_["admin_5"], disable_web_page_preview=True)
     await mute_on(chat_id)
-    await Alexa.mute_stream(chat_id)
+    await Alina.mute_stream(chat_id)
     await message.reply_text(
         _["admin_6"].format(message.from_user.mention), disable_web_page_preview=True
     )
