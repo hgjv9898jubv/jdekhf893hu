17aec6688c79fd853447e84c954a7639f776705e
iox7ssz <159122521+iox7ssz@users.noreply.github.com>
2024-03-08 22:14:39 +0300

    Update dev.py

--- AlinaXIQ/plugins/devs/dev.py
+++ AlinaXIQ/plugins/devs/dev.py
@@ -22,7 +22,7 @@ from time import time
 from pyrogram import filters
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, Message
 
-from AlexaMusic import app
+from AlinaXIQ import app
 from config import OWNER_ID
 
 
