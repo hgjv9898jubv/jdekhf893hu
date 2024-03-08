48c4333049d54a799da87b76d50a9ccb39d13fc6
iox7ssz <159122521+iox7ssz@users.noreply.github.com>
2024-03-08 23:27:25 +0300

    Update lyrics.py

--- AlinaXIQ/plugins/tools/lyrics.py
+++ AlinaXIQ/plugins/tools/lyrics.py
@@ -1,15 +1,3 @@
-# Copyright (C) 2024 by Alexa_Help @ Github, < https://github.com/TheTeamAlexa >
-# Subscribe On YT < Jankari Ki Duniya >. All rights reserved. © Alexa © Yukki.
-
-""""
-TheTeamAlexa is a project of Telegram bots with variety of purposes.
-Copyright (c) 2024 -present Team=Alexa <https://github.com/TheTeamAlexa>
-
-This program is free software: you can redistribute it and can modify
-as you want or you can collabe if you have new ideas.
-"""
-
-
 import random
 import re
 import string
@@ -20,8 +8,8 @@ from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, Message
 
 from config import BANNED_USERS, lyrical
 from strings import get_command
-from AlexaMusic import app
-from AlexaMusic.utils.decorators.language import language
+from AlinaXIQ import app
+from AlinaXIQ.utils.decorators.language import language
 
 ###Commands
 LYRICS_COMMAND = get_command("LYRICS_COMMAND")
