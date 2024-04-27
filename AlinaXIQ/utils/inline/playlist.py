bdcb0ec7a44d935f45a8e73765da8effb1a8e0e1
iox7ssz <159122521+iox7ssz@users.noreply.github.com>
2024-04-27 11:51:45 +0300

    Update playlist.py

--- AlinaXIQ/utils/inline/playlist.py
+++ AlinaXIQ/utils/inline/playlist.py
@@ -16,15 +16,12 @@ from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 def botplaylist_markup(_):
     buttons = [
         [
-            InlineKeyboardButton(
-                text=_["PL_B_1"],
-                callback_data="get_playlist_playmode",
-            ),
-            InlineKeyboardButton(text=_["PL_B_8"], callback_data="get_top_playlists"),
+            InlineKeyboardButton(text=_["S_B_6"], url=config.SUPPORT_CHANNEL),
         ],
         [
-            InlineKeyboardButton(text=_["PL_B_4"], callback_data="PM"),
-            InlineKeyboardButton(text=_["CLOSE_BUTTON"], callback_data="close"),
+            InlineKeyboardButton(
+                text=_["CLOSE_BUTTON"], callback_data="close"
+            ),
         ],
     ]
     return buttons
